5e3f3f35b9cd3333eda921b8f7e5dfb88fb5a08c
crome <crome@DESKTOP-BT6IRT2>
2022-09-12 11:33:58 +0200

    AA15 correcion

--- GITHUB/src/AA15/Main.java
+++ GITHUB/src/AA15/Main.java
@@ -102,8 +102,10 @@ public class Main {
 			    Jenkins.append("stages{ \r\n");
 			    Jenkins.append("stage('Main'){ \r\n");
 			    Jenkins.append("steps{ \r\n");
+			    Jenkins.append("script{ \r\n");
 			    Jenkins.append("def data = readFile(file:\"APIcapitales.txt\")\r\n");
 			    Jenkins.append("println(data) \r\n");
+			    Jenkins.append("} \r\n");
 			    Jenkins.append("} \r\n");
 			    Jenkins.append("} \r\n");
 			    Jenkins.append("} \r\n");
